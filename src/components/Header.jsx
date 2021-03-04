3a8d4d31b3f89bb35e1e3df5e1915278502baefa
Raul Luis <soyrauluis@outlook.com>
2021-03-04 19:14:06 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<header id='header'>
+			<h1 className='text-6xl font-bold text-center mt-8 text-white'>
+				BMI Calculator
+			</h1>
+			<h2 className=''>Keep track of your body mass index with ease!</h2>
+		</header>
+	);
+};
